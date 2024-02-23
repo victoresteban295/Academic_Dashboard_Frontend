52c83a83d89f366c104a5343f81a069d2968ea15
victoresteban295 <esteban.victor295@gmail.com>
2024-02-22 22:31:26 -0800

    Continue Working on Course Page

--- src/components/course/student/MainWidget/WeeklySchedule.js
+++ src/components/course/student/MainWidget/WeeklySchedule.js
@@ -1,6 +1,9 @@
 import { Box, Divider, Grid, Stack, Typography } from "@mui/material";
 
 const WeeklySchedule = ({ strDate, endDate }) => {
+
+    console.log("testing")
+
     return (
         <Stack
             sx={{
