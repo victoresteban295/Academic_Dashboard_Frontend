664893d6d9ea209f13f57f49eaf19887f9fd4a43
victoresteban295 <esteban.victor295@gmail.com>
2024-03-28 19:18:01 -0700

    Started Working on Student's Account Page

--- src/components/account/student/StudAccount.js
+++ src/components/account/student/StudAccount.js
@@ -1,7 +1,99 @@
+import { Alert, Snackbar, Stack, Typography } from "@mui/material";
+
 const StudAccount = () => {
+
+    /* State Value */
+    const [account, setAccount] = useState(accountInfo);
+    const changeAccount = (updatedAccount) => {
+        setAccount(updatedAccount);
+    }
+
+    /* Error Message Displaying in Alert */
+    const [errorMsg, setErrorMsg] = useState('');
+
+    /* Display Alert with Error Message */
+    const [openAlert, setOpenAlert] = useState(false);
+    const handleOpenAlert = (msg) => {
+        setErrorMsg(msg);
+        setOpenAlert(true);
+    }
+    const handleCloseAlert = () => {
+        setErrorMsg('');
+        setOpenAlert(false);
+    }
 
     return (
         <>
+            <Snackbar
+                open={openAlert}
+                anchorOrigin={{
+                    vertical: 'top', 
+                    horizontal: 'right',
+                }}
+                autoHideDuration={15000}
+                onClose={handleCloseAlert}
+            >
+                <Alert
+                    onClose={handleCloseAlert}
+                    severity="error"
+                    sx={{
+                        position: 'relative',
+                        color: 'text.primary',
+                        bgcolor: 'error.light',
+                        top: {
+                            fold: '0px',
+                            mobile: '0px',
+                            tablet: '50px',
+                            desktop: '50px',
+                        },
+                    }}
+                >
+                    {errorMsg}
+                </Alert>
+            </Snackbar> 
+            <Stack
+                spacing={2}
+                sx={{
+                    width: '100%',
+                    py: 1,
+                    px: 2,
+                }}
+            >
+                <Stack
+                    spacing={0}
+                >
+                    <Typography
+                        variant="h5"
+                        sx={{
+                            fontWeight: '700',
+                        }}
+                    >
+                        {"Student Account"}
+                    </Typography>
+                </Stack>
+                <Stack
+                    spacing={4}
+                    sx={{
+                        px: {
+                            fold: 0,
+                            mobile: 0,
+                            tablet: 2,
+                            desktop: 2,
+                        },
+                        width: '100%',
+                    }}
+                >
+                    <PersonalInformation
+                        department={account.department} 
+                        academicRole={account.academicRole}
+                        apptYear={account.apptYear}
+                        officeBuilding={account.officeBuilding}
+                        officeRoom={account.officeRoom}
+                        changeAccount={changeAccount}
+                        handleOpenAlert={handleOpenAlert}
+                    />
+                </Stack>
+            </Stack>
 
         </>
     )
